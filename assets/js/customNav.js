23c9c3b8787ccad682dfa18c3138f6130f2f76c8
Max Boog <max_boog@hotmail.com>
2021-05-02 23:00:24 +0200

    home page design

--- assets/js/customNav.js
+++ assets/js/customNav.js
@@ -5,8 +5,9 @@ class CustomNav extends HTMLElement {
         
         <nav class="navbar navbar-custom navbar-light fixed-top shadow">
             <div class="container">
-            <a class="navbar-brand" href="/"><img class="img-logo" width="180"  src="/assets/images/logo.png" alt="Takacs & Verheijden"/></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <!-- <a class="navbar-brand" href="/"><img class="img-logo" width="45"  src="/assets/images/paper-boat.svg" alt="Takacs & Verheijden"/></a> -->
+                <a class="navbar-brand" href="/">Takacs <span class="text-color-secondary"> &amp;</span> Verheijden</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse " id="navbarSupportedContent">

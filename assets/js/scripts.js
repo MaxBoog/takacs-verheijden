dfe17095ee7b70d33e138078097cc2f5eab3d4c1
Max Boog <maxboog@MacBook-Air-van-Max.local>
2022-01-24 16:56:58 +0100

    promotie toegevoegd

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,40 +1,30 @@
-// opening/closing the navbar
-
-function openNav() {
-  document.getElementById("nav").style.height = "100%";
-}
-
-function closeNav() {
-  document.getElementById("nav").style.height = "0%";
-}
-
 // expanding/shrinking the navbar
 
 window.onscroll = function () {
-  scrollFunction();
-};
+  scrollFunction()
+}
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "1.5rem 2rem";
+    document.getElementById("navbar").style.padding = "1.5rem 2rem"
   } else {
-    document.getElementById("navbar").style.padding = "2rem 2rem";
+    document.getElementById("navbar").style.padding = "2rem 2rem"
   }
 }
 
 // scroll to top button
 
 function scrollToTop() {
-  window.scroll({ top: 0, left: 0, behavior: "smooth" });
+  window.scroll({ top: 0, left: 0, behavior: "smooth" })
 }
 
 // close navbar on click
 
-const navLinks = document.querySelectorAll(".dropdown-item");
-const menuToggle = document.getElementById("navbarSupportedContent");
-const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
+const navLinks = document.querySelectorAll(".dropdown-item")
+const menuToggle = document.getElementById("navbarSupportedContent")
+const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false })
 navLinks.forEach((l) => {
   l.addEventListener("click", () => {
-    bsCollapse.toggle();
-  });
-});
+    bsCollapse.toggle()
+  })
+})

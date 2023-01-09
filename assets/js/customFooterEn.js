1facec92bc81a2bd68b3754562d6872c7604e7a7
Max Boog <maxboog@MacBook-Air-van-Max.local>
2023-01-09 20:32:13 +0100

    english version

---
+++ assets/js/customFooterEn.js
@@ -0,0 +1,53 @@
+class CustomFooter extends HTMLElement {
+  constructor() {
+    const datum = new Date().getFullYear().toString();
+    super();
+    this.innerHTML = `
+        <footer>
+            <div class="container padding-y footer">
+                <div class="row justify-content-start">
+                    <div class="col-lg-4">
+                        <h3 class="font-secondary pt-5 pt-md-0">Takacs <span class="text-color-secondary"> &amp;</span> Verheijden BV</h3>
+                        <!-- <p class="footer-quote d-flex mx-auto"></p> -->
+                        <p><i class="fas fa-chevron-right text-color-secondary"></i><a class="ps-2" href="/assets/docs/Algmene voorwaarden Takacs& Verheijden B.V. .pdf">Terms of Service</a></p>
+                        <p><i class="fas fa-chevron-right text-color-secondary"></i><a class="ps-2" href="/assets/docs/Klachtenregeling Takacs & Verheijden.pdf">Complaints</a></p>
+                        <p><i class="fas fa-chevron-right text-color-secondary"></i><a class="ps-2" href="/assets/docs/Privacyverklaring.pdf">Privacy statement</a></p>
+                    </div>
+                    <div class="col-lg-4">
+                        <h3 class="font-secondary pt-5 pt-md-0">Sitemap</h3>
+                        <p><i class="fas fa-chevron-right text-color-secondary"></i><a class="ps-2" href="/en/index.html">Home</a></p>
+                        <p><i class="fas fa-chevron-right text-color-secondary"></i><a class="ps-2" href="/en/leadership.html">Academic Leadership</a></p>
+                        <p><i class="fas fa-chevron-right text-color-secondary"></i><a class="ps-2" href="/en/about-us.html">About Us</a></p>
+                        <p><i class="fas fa-chevron-right text-color-secondary"></i><a class="ps-2" href="/en/contact.html">Contact</a></p>
+
+                    </div>
+                    <div class="col-lg-4">
+                        <h3 class="font-secondary pt-5 pt-md-0">Contact</h3>
+                        <p>Barbara Takacs</p>
+                        <p><a href="tel:+31624156731">+ 31 6 241 56731</a></p>
+                        <p><a href="mailto:mail@barbaratakacs.nl">mail@barbaratakacs.nl</a></p>
+                        <p>Molenaerstraat 18</p>
+                        <p class="pb-2">2023 EK Haarlem</p>
+                    </div>
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col">
+                        <div class="copyright">
+                            <p>&copy; ${datum} - <a href="https://www.takacs-verheijden.nl/en/">takacs-verheijden.nl</a></p>
+                            <!-- <p>Wij volgen de klachtenprocedure zoals beschreven door <a href="https://nobtra.nl/opdrachtgevers/klachtenprocedure" target="_blank"><u>NOBTRA</u></a></p> -->
+                            <p class="pt-2 dbw">a design by: <a href="https://www.delbellowebdesign.nl" target="_blank">delbellowebdesgin.nl</a></p>
+                            <p class="icon-note pt-2">icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </footer>
+
+      
+      `;
+  }
+}
+
+window.customElements.define("custom-footer", CustomFooter);
--- assets/js/customNavEn.js
+++ assets/js/customNavEn.js
@@ -7,7 +7,7 @@ class CustomNav extends HTMLElement {
             <div class="container">
                 <!-- <a class="navbar-brand" href="/"><img class="img-logo" width="45"  src="/assets/images/paper-boat.svg" alt="Takacs & Verheijden"/></a> -->
 
-                <a class="navbar-brand" href="/">Takacs <span class="text-color-secondary"> &amp;</span> Verheijden</a>
+                <a class="navbar-brand" href="/en/index.html">Takacs <span class="text-color-secondary"> &amp;</span> Verheijden</a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -16,27 +16,27 @@ class CustomNav extends HTMLElement {
                 <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                            <a class="nav-link active" aria-current="page" href="/en/index.html">Home</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="/leiderschap-in-de-academie.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Leiderschap in de academie
+                            <a class="nav-link dropdown-toggle" href="/en/leadership.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Academic Leadership
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="/leiderschap-in-de-academie.html#leiderschapsontwikkeling">Leiderschapsontwikkeling</a></li>
-                                <li><a class="dropdown-item" href="leiderschap-in-de-academie.html#intervisie">Intervisie</a></li>
-                                <li><a class="dropdown-item" href="leiderschap-in-de-academie.html#coaching">Coaching</a></li>
-                                <li><a class="dropdown-item" href="leiderschap-in-de-academie.html#advies">Advies</a></li>
+                                <li><a class="dropdown-item" href="/en/leadership.html#developing-leadership">Developing Leadership</a></li>
+                                <li><a class="dropdown-item" href="/en/leadership.html#intervision">Intervision</a></li>
+                                <li><a class="dropdown-item" href="/en/leadership.html#coaching">Coaching</a></li>
+                                <li><a class="dropdown-item" href="/en/leadership.html#advice">Advice</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/over-ons.html">Over ons</a>
+                            <a class="nav-link" href="/en/about-us.html">About Us</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/contact.html">Contact</a>
+                            <a class="nav-link" href="/en/contact.html">Contact</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/en/index.html">EN</a>
+                            <a class="nav-link" href="/index.html">NL</a>
                         </li>
                     </ul>
                 </div>
